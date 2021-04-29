4351c4e6867baf0431f2ea32aebc41081c2092a9
jimtheflash <jim.kloet@gmail.com>
2021-04-29 10:40:34 -0500

    update workflows

---
+++ inst/LOCAL_update_dash.R
@@ -0,0 +1,3 @@
+source('./inst/get_gambling_stuff_data_LOCAL.R')
+source('./inst/get_dashboard_props_data.R')
+source('./inst/output_dashboard_data.R')
---
+++ inst/SERVER_update_dash.R
@@ -0,0 +1,3 @@
+source('./inst/get_gambling_stuff_data_SERVER.R')
+source('./inst/get_dashboard_props_data.R')
+source('./inst/output_dashboard_data.R')
