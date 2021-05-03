e670102df79598ac5489783d4c01e5f55eb545c5
jimtheflash <jim.kloet@gmail.com>
2021-05-03 14:14:44 -0500

    fix opponent matching

--- inst/get_gambling_stuff_data_SERVER.R
+++ inst/get_gambling_stuff_data_SERVER.R
@@ -24,13 +24,19 @@ player_data <-
              tidyplayer = normalize_names(PLAYER_NAME, key = system.file('lu', 'nba', 'player', 'lu.json', package = 'betfinder'))) %>%
       select(-PLAYER_NAME, -TEAM_ABBREVIATION),
     by = c('tidyteam', 'tidyplayer')) %>%
+  # this gets us players whether they're playing or not, so can filter out the palyers who are not
+  filter(!is.na(MATCHUP)) %>%
   # looks like the lineups can have duplicated players if someone is a tossup to start, so just keep the most informative row
+  group_by(MATCHUP) %>%
+  fill(tidyteam, .direction = 'updown') %>%
+  mutate(teams = list(unique(tidyteam)),
+         team1 = lapply(teams, '[[', 1),
+         team2 = lapply(teams, '[[', 2),
+         tidyopp = if_else(tidyteam == team1, team2, team1)) %>%
+  ungroup() %>%
   group_by(tidyplayer) %>%
   arrange(desc(LINEUP_DESC)) %>%
   filter(row_number() == 1) %>%
-  ungroup() %>%
-  group_by(MATCHUP) %>%
-  mutate(tidyopp = rev(tidyteam)) %>%
   ungroup() %>%
   transmute(
     tidyplayer, tidyteam, tidyopp,
