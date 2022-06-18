87eb3ac6db2455cbb238f921fbc197c8802fa5c7
jimtheflash <jim.kloet@gmail.com>
2022-06-18 13:53:51 -0500

    remove a bunch of superfluous stuff for the old dashboard in the package

--- inst/LOCAL_update_dash.R
+++
@@ -1,7 +0,0 @@
-startTime <- Sys.time()
-devtools::load_all()
-data_list <- get_data_for_dashboard('local')
-props_list <- get_all_props()
-output_data_for_dashboard(data_list, props_list)
-endTime <- Sys.time()
-endTime-startTime
